2ef80dcb1b7a67ee8362f974d20e16496b1dfaed
a1103ti <a1103ti@aiit.ac.jp>
2011-12-22 20:26:55 +0900

    Initial commit

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+set :application, "blog"
+set :deploy_to, "/home/rails/blog"
+set :repository,  "git@github.com:a1103ti/blog"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+
+role :web, "localhost"                          # Your HTTP server, Apache/etc
+role :app, "localhost"                          # This may be the same as your `Web` server
+role :db,  "localhost", :primary => true # This is where Rails migrations will run
+#role :db,  "your slave db-server here"
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+namespace :deploy do
+  task :start do ; end
+   task :stop do ; end
+   task :restart, :roles => :app, :except => { :no_release => true } do
+     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+   end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '74ba9cf32b25ce7764699d581cd9cc3b79d58508354f059c85cf97c7d98d3fbfa4276021cc371349c048b628d54295be76733d7670b44ad1646fbec98c6a9304'
